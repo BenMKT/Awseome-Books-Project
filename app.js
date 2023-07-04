806e0458d28717dd895b825d6f21dbe4c1f2981a
Gabriel Y <gabriel.yrigoyen43@gmail.com>
2023-07-04 16:07:04 -0600

    Ajust function to make them methods
    Feature not finished

--- app.js
+++ app.js
@@ -4,28 +4,19 @@ class Book {
     this.author = author;
   }
 
-
   bookHTML() {
     const bookSection = document.querySelector("#library");
     bookSection.innerHTML = "";
-    let bookKLibrary = [];
+
+    let bookKLibrary = JSON.parse(localStorage.getItem("jsonLibrary"));
     bookKLibrary.forEach((n) => {
       bookSection.innerHTML += `<div>
-        <p>${n.title}</p>
-        <p>${n.author}</p>
-        <button type='button' onclick=remove('${n.title}','${n.author}') >Remove</button>
-        <hr>
-    </div>`;
-    });
-  }
-
-  addBook() {
-    const titleText = document.querySelector("#title");
-    const authorText = document.querySelector("#author");
-    const book = new Book(titleText.value, authorText.value);
-    bookKLibrary.push(book);
-    localStorage.setItem("jsonLibrary", JSON.stringify(bookKLibrary));
-    bookHTML();
+          <p>${n.title}</p>
+          <p>${n.author}</p>
+          <button type='button' onclick=remove('${n.title}','${n.author}') >Remove</button>
+          <hr>
+      </div>`;
+      });
   }
 
   remove(title, author) {
@@ -45,60 +36,34 @@ class Book {
 const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  const titleText = document.querySelector("#title");
+  const authorText = document.querySelector("#author");
+
+  const book = new Book(titleText.value, authorText.value);
+  let bookKLibrary = JSON.parse(localStorage.getItem("jsonLibrary"));
+  bookKLibrary.push(book);
+  localStorage.setItem("jsonLibrary", JSON.stringify(bookKLibrary));
+
+  bookHTML();
+
   form.reset();
 });
 
 window.addEventListener("load", () => {
   if (localStorage.getItem("jsonLibrary")) {
-    bookKLibrary = JSON.parse(localStorage.getItem("jsonLibrary"));
-    bookHTML();
+
+    const bookSection = document.querySelector("#library");
+    bookSection.innerHTML = "";
+
+    let bookKLibrary = JSON.parse(localStorage.getItem("jsonLibrary"));
+    bookKLibrary.forEach((n) => {
+      bookSection.innerHTML += `<div>
+          <p>${n.title}</p>
+          <p>${n.author}</p>
+          <button type='button' onclick=remove('${n.title}','${n.author}') >Remove</button>
+          <hr>
+      </div>`;
+      });
+    
   }
 });
-
-
-// let bookKLibrary = [];
-// // eslint-disable-next-line
-// function bookHTML() {
-//   const bookSection = document.querySelector('#library');
-//   bookSection.innerHTML = '';
-//   bookKLibrary.forEach((n) => {
-//     bookSection.innerHTML += `<div>
-//         <p>${n.title}</p>
-//         <p>${n.author}</p>
-//         <button type='button' onclick=remove('${n.title}','${n.author}') >Remove</button>
-//         <hr>
-//     </div>`;
-//   });
-// }
-// // eslint-disable-next-line
-// function addBook() {
-//   const titleText = document.querySelector('#title');
-//   const authorText = document.querySelector('#author');
-//   const book = new Book(titleText.value, authorText.value);
-//   bookKLibrary.push(book);
-//   localStorage.setItem('jsonLibrary', JSON.stringify(bookKLibrary));
-//   bookHTML();
-// }
-// // eslint-disable-next-line
-// function remove(title, author) {
-//   for (let i = 0; i < bookKLibrary.length; i += 1) {
-//     if (bookKLibrary[i].title === title && bookKLibrary[i].author === author) {
-//       bookKLibrary.splice(i, 1);
-//     }
-//   }
-//   localStorage.setItem('jsonLibrary', JSON.stringify(bookKLibrary));
-//   bookHTML();
-// }
-
-// const form = document.querySelector('form');
-// form.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   form.reset();
-// });
-
-// window.addEventListener('load', () => {
-//   if (localStorage.getItem('jsonLibrary')) {
-//     bookKLibrary = JSON.parse(localStorage.getItem('jsonLibrary'));
-//     bookHTML();
-//   }
-// });
